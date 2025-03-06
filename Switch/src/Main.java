1bc90f629a6690e1f9c6f9752d4b36e92d0f8b54
Kesavaraja M <krkesavaraja@gmail.com>
2025-03-06 18:42:32 +0530

    Initial commit

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,49 +1,8 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-//        String fruit = input.next();
-//
-//        switch (fruit) {
-//            case "Mango" -> System.out.println("Yellow");
-//            case "Apple" -> System.out.println("Apple");
-//            case "Guava" -> System.out.println("Green");
-//            default -> System.out.println("Enter a Valid Fruit ");
-//        }
-
-        int day = input.nextInt();
-//        switch (day)
-//        {
-//            case 1 -> System.out.println("Monday");
-//            case 2 -> System.out.println("Tuesday");
-//            case 3 -> System.out.println("Wednesday");
-//            case 4 -> System.out.println("Thursday");
-//            case 5 -> System.out.println("Friday");
-//            case 6 -> System.out.println("Saturday");
-//            case 7 -> System.out.println("Sunday");
-//            default -> System.out.println("Enter Number between 1 to 7 :");
-
-//        switch (day) {
-//            case 1:
-//            case 2:
-//            case 3:
-//            case 4:
-//            case 5:
-//                System.out.println("Weekday");
-//                break;
-//            case 6:
-//            case 7:
-//                System.out.println("Weekend");
-//                break;
-//
-//        }
-        switch (day) {
-            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
-            case 6, 7 -> System.out.println("Weekend");
-        }
 
 
     }
